aa4badad50d4e3f6ec640cf2ff06d123b310daf4
RayiemasMP <masrayie@gmail.com>
2021-04-17 23:06:39 +0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BibitNodeJSTest
 Test Bibit Node JS Developer
+
+Access SearchMovies:
+- /search
+	- URL : https://searchmoviebibitrayiemas.herokuapp.com/movies/search?apikey=faf7e5bb&s=Batman&page=10
+- /detail
+	- URL : https://searchmoviebibitrayiemas.herokuapp.com/movies/detail?apikey=faf7e5bb&t=Batman
+- /api/apilogs
+	- URL: https://searchmoviebibitrayiemas.herokuapp.com/api/apilogs
--- Search Movies/db/connection.js
+++ Search Movies/db/connection.js
@@ -1,11 +1,22 @@
+// const dbConn = require('knex')({
+//     client: 'mysql',
+//     connection: {
+//       host : '127.0.0.1',
+//       user : 'root',
+//       password : '',
+//       database : 'db_search_movie_bibit'
+//     }
+// })
+
 const dbConn = require('knex')({
-    client: 'mysql',
-    connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : '',
-      database : 'db_search_movie_bibit'
-    }
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : '5433',
+    user : 'postgres',
+    password : 'adminsuper123',
+    database : 'db_search_movies_bibit'
+  }
 })
 
 module.exports = dbConn
