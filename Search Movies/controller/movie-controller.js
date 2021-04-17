5894ecf2b0af2212ce9438d4717b6be65d74dca3
RayiemasMP <masrayie@gmail.com>
2021-04-17 14:46:00 +0700

    Find Detail Movie

--- Search Movies/controller/movie-controller.js
+++ Search Movies/controller/movie-controller.js
@@ -4,6 +4,8 @@ const axios = require('axios')
 const APIKEY = 'faf7e5bb'
 const URL = 'https://www.omdbapi.com'
 
+const defaultParams = ['type', 'y', 'plot', 'v', 'callback', 'apikey']
+
 const getAPI = (fullURL, key, query) => {
     return new Promise(callback => {
         try {
@@ -30,17 +32,36 @@ const getAPI = (fullURL, key, query) => {
 }
 
 const getMoviesBySearch = async (req, res) => {
+    // Collect Query
     const query = req.query !== undefined ? req.query : {}
+    
+    const acceptedParams = [...defaultParams, 's', 'page']
+
+    // Get API Key & Filter Params
     let apikey
     if (Object.keys(query).length) {
         apikey = query.apikey
+        const notAcceptableParams = Object.keys(query).filter((d) => !acceptedParams.includes(d))
+        if (notAcceptableParams.length) {
+            return res.status(400).send({
+                error: {
+                    code: 400,
+                    message: "['" + notAcceptableParams.join("', '") + "'] => Params are not recognized for this endpoint."
+                }
+            })
+        }
     }
+
     try {
+        // Try to hit API
         const data = await getAPI(URL, apikey, query)
         let dataMovies = data.data
+
+        // Check if Err
         if (dataMovies.err) {
             res.status(200).send({err: dataMovies.err})
         } else {
+            // Response of success
             res.status(200).send({data: dataMovies.Search, totalResults: dataMovies.totalResults, Response: dataMovies.Response})
         }
     } catch (e) {
@@ -54,6 +75,51 @@ const getMoviesBySearch = async (req, res) => {
     }
 }
 
+const getMoviesDetail = async (req, res) => {
+     // Collect Query
+     const query = req.query !== undefined ? req.query : {}
+    
+     const acceptedParams = [...defaultParams, 't', 'i']
+ 
+     // Get API Key & Filter Params
+     let apikey
+     if (Object.keys(query).length) {
+         apikey = query.apikey
+         const notAcceptableParams = Object.keys(query).filter((d) => !acceptedParams.includes(d))
+         if (notAcceptableParams.length) {
+             return res.status(400).send({
+                 error: {
+                     code: 400,
+                     message: "['" + notAcceptableParams.join("', '") + "'] => Params are not recognized for this endpoint."
+                 }
+             })
+         }
+     }
+ 
+     try {
+         // Try to hit API
+         const data = await getAPI(URL, apikey, query)
+         let dataMovies = data.data
+ 
+         // Check if Err
+         if (dataMovies.err) {
+             res.status(200).send({err: dataMovies.err})
+         } else {
+             // Response of success
+             res.status(200).send({data: dataMovies})
+         }
+     } catch (e) {
+         console.log(e)
+         res.status(500).send({
+             error: {
+                 code: 500,
+                 message: e.message
+             }
+         })
+     }
+}
+
 module.exports = {
-    getMoviesBySearch: getMoviesBySearch
+    getMoviesBySearch: getMoviesBySearch,
+    getMoviesDetail: getMoviesDetail
 }
--- Search Movies/router/movies-router.js
+++ Search Movies/router/movies-router.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const {getMoviesBySearch} = require('../controller/movie-controller')
+const {getMoviesBySearch, getMoviesDetail} = require('../controller/movie-controller')
 const router = express.Router()
 
 router.use(express.json())
 
 // GET
 router.route('/search').get(getMoviesBySearch)
+router.route('/detail').get(getMoviesDetail)
 // router.route('/aspAssignment/aspassign/:id').get(tokenAuth(), getAspAssignById)
 
 module.exports = router
