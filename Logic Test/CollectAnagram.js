65694d71bb3ea4f94315fae7f1dc9e86586ff619
RayiemasMP <masrayie@gmail.com>
2021-04-17 21:09:19 +0700

    Logic Test Update

--- Logic Test/CollectAnagram.js
+++ Logic Test/CollectAnagram.js
@@ -1,24 +1,25 @@
 function collectAnagram (arrString) {
-    const data = arrString.slice(0)
+    const data = arrString.slice(0).sort((a,b) => a.length - b.length)
     const anagrams = []
     while (data.length > 0) {
         let word = data[0]
         let idxpop = []
         let group = []
         for (i in data) {
-            if (word.length === data[i].length) {
+            let arrData = data[i].split('')
+            let arrWord = word.split('')
+            if (word.length === data[i].length && arrData.every((d) => arrWord.includes(d))) {
                 idxpop.push(i)
                 group.push(data[i])
             }
         }
-        if (idxpop.length) {
-            for (idx of idxpop) {
-                data.splice(idx, 1)
+        if (group.length) {
+            for (wrd of group) {
+                let idxNow = data.indexOf(wrd)
+                data.splice(idxNow, 1)
             }
         }
         anagrams.push(group)
     }
     return anagrams
 }
-
-console.log(collectAnagram(['kita', 'atik', 'tika', 'aku', 'kia', 'makan', 'kua']))
